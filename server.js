607312cb1ca156c0415e0758352cb032ea1c5212
kasta-sports-tv <kasta.sports.tv@gmail.com>
2026-05-01 00:39:45 +0300

    Initialize Express server with playlist endpoint

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const base = "http://94.156.59.233:8899/udp/239.10.2.";
+const port = ":30000";
+
+app.get("/playlist", (req, res) => {
+  let m3u = "#EXTM3U\n\n";
+
+  for (let i = 1; i <= 200; i++) {
+    m3u += `#EXTINF:-1,Channel ${i}\n${base}${i}${port}\n\n`;
+  }
+
+  res.setHeader("Content-Type", "application/x-mpegURL");
+  res.send(m3u);
+});
+
+app.listen(PORT, () => {
+  console.log("Running on", PORT);
+});
